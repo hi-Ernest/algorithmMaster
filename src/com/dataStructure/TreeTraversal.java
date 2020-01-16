eed221fccf39c5dc232605ae4946fe1297991f2b
ruihuachen <chenhr1130@163.com>
2020-01-16 23:51:44 +0800

    update maxDepth of tree problem

--- src/com/dataStructure/TreeTraversal.java
+++ src/com/dataStructure/TreeTraversal.java
@@ -100,6 +100,11 @@ public class TreeTraversal {
         return res;
     }
 
+    /**
+     * BFS遍历
+     * @param root
+     * @param res
+     */
     private void bfs(TreeNode root, List<List<Integer>> res) {
         Queue<TreeNode> queue = new LinkedList<>();
         if (root != null)
@@ -118,5 +123,68 @@ public class TreeTraversal {
         }
     }
 
+    /**
+     * 二叉树最大深度
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    private static int maxDepth1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        //BFS的层次遍历思想，记录二叉树的层数，
+        //遍历完，层数即为最大深度
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int maxDepth = 0;
+        while (!queue.isEmpty()) {
+            maxDepth++;
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.pollFirst();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return maxDepth;
+    }
+
+//    private static int maxDepth2(TreeNode root) {
+//        if (root == null) {
+//            return 0;
+//        }
+//        LinkedList<Pair<TreeNode, Integer>> stack = new LinkedList<>();
+//        stack.push(new Pair<>(root, 1));
+//        int maxDepth = 0;
+//        //DFS实现前序遍历，每个节点记录其所在深度
+//        while (!stack.isEmpty()) {
+//            Pair<TreeNode, Integer> pair = stack.pop();
+//            TreeNode node = pair.first;
+//            //DFS过程不断比较更新最大深度
+//            maxDepth = Math.max(maxDepth, pair.second);
+//            //记录当前节点所在深度
+//            int curDepth = pair.second;
+//            //当前节点的子节点入栈，同时深度+1
+//            if (node.right != null) {
+//                stack.push(new Pair<>(node.right, curDepth + 1));
+//            }
+//            if (node.left != null) {
+//                stack.push(new Pair<>(node.left, curDepth + 1));
+//            }
+//        }
+//        return maxDepth;
+//    }
 
 }
