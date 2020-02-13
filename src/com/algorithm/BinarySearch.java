912e72a48b5c341a221492ffe9f7cce21164b456
ruihuachen <chenhr1130@163.com>
2020-02-13 23:59:59 +0800

    update some

--- src/com/algorithm/BinarySearch.java
+++ src/com/algorithm/BinarySearch.java
@@ -21,4 +21,21 @@ public class BinarySearch {
         }
         return res;
     }
+
+    private int binarySearch_2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] == target) {
+                result = mid;
+                break;
+            }else if (nums[mid] > target) {
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
 }
